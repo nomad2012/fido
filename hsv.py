0a4e0a7b0c38bd78e625597e40a7216adcc93868
Dennis Draheim <nomadicnerd@gmail.com>
2013-07-08 18:09:26 -0500

    cv2 interface working

--- hsv.py
+++ hsv.py
@@ -5,8 +5,8 @@
 # achuwilson.wordpress.com
 import cv
 import time
-x_co = 0
-y_co = 0
+x_co = 160
+y_co = 120
 def on_mouse(event,x,y,flag,param):
   global x_co
   global y_co
@@ -15,8 +15,11 @@ def on_mouse(event,x,y,flag,param):
     y_co=y
 
 cv.NamedWindow("camera", 1)
+cv.MoveWindow("camera", 0, 0)
 capture = cv.CaptureFromCAM(0)
 font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 0.5, 1, 0, 2, 8)
+mins = [255, 255, 255]
+maxs = [0, 0, 0]
 while True:
     src = cv.QueryFrame(capture)
     cv.Smooth(src, src, cv.CV_BLUR, 3)
@@ -25,8 +28,18 @@ while True:
     cv.CvtColor(src, hsv, cv.CV_BGR2HSV)
     cv.SetMouseCallback("camera",on_mouse, 0);
     s=cv.Get2D(hsv,y_co,x_co)
+    maxs = map(max, maxs, s)
+    mins = map(min, mins, s)
     print "H:",s[0],"      S:",s[1],"       V:",s[2]
+    print "min = {}, max = {}".format(mins, maxs)
     cv.PutText(src,str(s[0])+","+str(s[1])+","+str(s[2]), (x_co,y_co),font, (55,25,255))
     cv.ShowImage("camera", src)
-    if cv.WaitKey(10) == 27:
+    k = cv.WaitKey(10)
+    if k != -1:
+        print "key = {}".format(k)
+    if k == ord('r'):
+        maxs = [0,0,0]
+        mins = [255,255,255]
+    if k == 27:
         break
+
